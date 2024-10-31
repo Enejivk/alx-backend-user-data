524c6108329c05903b1a2da70e287ee8bb25d3b5
enejivk <enejivic@gmail.com>
2024-10-31 20:09:09 +0100

    filtering the user information

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module for obfuscating log messages
+"""
 import re
-from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
-    """ Returns regex obfuscated log messages """
-    for field in fields:
-        message = re.sub(f'{field}=(.*?){separator}',
-                         f'{field}={redaction}{separator}', message)
-    return message
+
+def filter_datum(fields, redaction, message, separator):
+    """
+    Returns the log message obfuscated
+    Args:
+        fields: a list of strings representing all fields to obfuscate
+        redaction: a string representing by what the field will be obfuscated
+        message: a string representing the log line
+        separator: a string representing the character separating fields
+    Returns:
+        The obfuscated log message
+    """
+    return re.sub(f'({"|".join(fields)})=[^{separator}]*', f'\\1={redaction}', message)
